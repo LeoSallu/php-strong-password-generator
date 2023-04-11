613ad1093efba78e265d44ad6e6e8095cad0e20f
LeoSallu <L.sallustio99@gmail.com>
2023-04-11 16:52:39 +0200

    inserito bootstrap

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 $number = $_GET['input'];
 function randomPassword($number)
 {
-    $letter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $special = '!@#$%^&*()_+-={}[]|:;<>,.?/';
+    $letter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-={}[]|:;<>,.?/';
     $pass = array();
-    $length = strlen($letter) - 1; 
-    for ($i = 0; $i < $number ; $i++) {
+    $length = strlen($letter) - 1;
+    for ($i = 0; $i < $number; $i++) {
         $n = rand(0, $length);
         $pass[] = $letter[$n];
-    }
-    return implode($pass); 
+    };
+    return implode($pass);
 }
 ?>
 <!DOCTYPE html>
@@ -21,18 +20,35 @@ function randomPassword($number)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
+    <!-- my css  -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- bootstrap cdn  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
-    <form action="index.php" method="GET">
-        <label for="input"><h4>Inserisci il numero di caratteri della tua password con un massimo di 16 caratteri</h4></label>
-        <input name="input" type="number">
-        <button type="submit">Invia</button>
-    </form>
-    <?php if ($number <= 16 && $number > 0) { ?>
-        <h1>La tua password è </h1>
-        <p><?php echo randomPassword($number);?></p>
-        <?php } else {?>
-            <p>Il numero di caratteri non è valido</p>
-        <?php } ?>
+    <div id="background">
+        <div class="container">
+            <h1>Generatore di password complesse random</h1>
+            <form action="index.php" method="GET" class="w-50">
+                <label for="input" class="form-label">Inserisci il numero di caratteri della tua password</label>
+                <input name="input" type="number" class="form-control">
+                <div id="passwordHelpBlock" class="form-text text-light">La tua password può contentere da un minimo di 5 carattere a un massimo di 20</div>
+                <button type="submit" class="btn btn-primary">Invia</button>
+            </form>
+            <div class="my-2 text-center">
+                <?php if ($number <= 16 && $number > 0) { ?>
+                    <h3>La tua password è </h3>
+                    <p><?php echo randomPassword($number); ?></p>
+                <?php } else { ?>
+                    <div class="alert alert-info w-25" role="alert">
+                        <p>Il numero di caratteri non è valido</p>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
+
 </html>
